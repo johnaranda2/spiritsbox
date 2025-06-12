5a0319fdb4e38ebf17cba5f19f7e1524f0e553a4
johnaranda <john.aranda@gmail.com>
2025-06-11 21:32:28 -0700

    Actualizo requirementsl

--- Test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCOw6xri88klWPnvbWtl2v_IqZxtV6Hcvo")  # Reemplaza aquí si no usas dotenv
-
-models = genai.list_models()
-for model in models:
-    print(model.name, "→", model.supported_generation_methods)
