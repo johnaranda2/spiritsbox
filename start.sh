e41c1da6ba121d8de53e4eb7b55b4b2c28debce8
johnaranda <john.aranda@gmail.com>
2025-06-11 21:57:29 -0700

    Actualizo eliminar zip

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Activar entorno virtual si es necesario
+# source venv/bin/activate
+
+# Exportar variables necesarias para Flask
 export FLASK_APP=app.py
+export FLASK_ENV=production
+export FLASK_RUN_PORT=3000  # Glitch y otros usan el puerto 3000
 export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=3000
+
+# Iniciar la aplicación
 flask run
